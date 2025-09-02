c3365910adbc09a8c845817a3a72ee0e17b54fca
arthur <arthurcvleao@gmail.com>
2025-09-02 16:00:54 -0300

    Create getUserByUserDetails method inside UserService

--- src/main/java/com/todolist/user/service/UserService.java
+++ src/main/java/com/todolist/user/service/UserService.java
@@ -1,23 +1,34 @@
 package com.todolist.user.service;
 
 
+import com.todolist.exceptions.UserNotFoundException;
+import com.todolist.user.model.User;
 import com.todolist.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
-    @Autowired
-    UserRepository userRepository;
-
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByLogin(username);
     }
 
+
+    public User getUserByUserDetails(UserDetails userDetails) {
+        return userRepository.findUserByLogin(userDetails.getUsername())
+                .orElseThrow(() ->
+                        new UserNotFoundException("There was a error retrieving the user stored inside the bearer token",
+                        userDetails.getUsername()));
+    }
 }

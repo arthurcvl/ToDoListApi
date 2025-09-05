ead7f3d14dbc9e9a5d2dc6d99ccff24853a6800f
arthur <arthurcvleao@gmail.com>
2025-09-05 17:59:03 -0300

    test: add unit tests for TaskService

--- src/main/java/com/todolist/task/dto/TaskDetailsDto.java
+++ src/main/java/com/todolist/task/dto/TaskDetailsDto.java
@@ -2,8 +2,10 @@ package com.todolist.task.dto;
 
 import com.todolist.task.enums.TaskPriority;
 import com.todolist.task.enums.TaskState;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
+@Builder
 public record TaskDetailsDto(Long id, String name, String description, TaskState taskState, TaskPriority taskPriority, LocalDate expiresAt) {
 }

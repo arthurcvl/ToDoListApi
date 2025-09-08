5452fe9a64c5480050f711e02db78a7c6c9b782a
arthur <arthurcvleao@gmail.com>
2025-09-08 16:11:14 -0300

    Made swagger-ui.html public and deleted email component

--- src/main/java/com/todolist/security/SecurityConfigurations.java
+++ src/main/java/com/todolist/security/SecurityConfigurations.java
@@ -30,8 +30,11 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                                .requestMatchers("**/swagger-ui/**", "/v3/api-docs/**").permitAll()
-
+                                .requestMatchers(
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**"
+                                ).permitAll()
                                 .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
